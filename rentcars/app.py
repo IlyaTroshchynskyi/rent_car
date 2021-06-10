65511908c9577461659e675a7cb50ae157c9a5a9
Ilya <itroshchinskiy@rambler.ua>
2021-06-10 20:44:20 +0300

    added migration class

--- rentcars/app.py
+++ rentcars/app.py
@@ -1,15 +1,11 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from config import Configuration
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 db = SQLAlchemy(app)
-
-
-# @app.route('/', methods=["GET"])
-# def index():
-#     return render_template('index.html')
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
+migrate = Migrate(app, db)
+from models import *
