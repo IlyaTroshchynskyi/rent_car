c94af41da96cd381904ca663a1ed6119cf880e3f
Ilya <itroshchinskiy@rambler.ua>
2021-06-10 20:44:52 +0300

    changed fields

--- rentcars/forms.py
+++ rentcars/forms.py
@@ -23,3 +23,31 @@ class AddOrder(FlaskForm):
     def validate_order_date(form, field):
         if not field.data > datetime.date.today():
             raise ValidationError('The day must be no less then current')
+
+
+class AddCar(FlaskForm):
+
+    car_number = StringField('car number', validators=[InputRequired(
+        message="Data for car number have to be provided")])
+    car_description = StringField('car description', validators=[InputRequired(),
+        Length(min=3, max=90, message="Minimum length 3 maximum length of 90 characters")])
+    rental_cost = FloatField('rental cost', validators=[InputRequired(),
+        NumberRange(min=1, message="Minimal value is one day")])
+
+
+    submit = SubmitField('Add Car')
+
+
+class AddClient(FlaskForm):
+    first_name = StringField('first name', validators=[InputRequired(
+        message="Data for car number have to be provided")])
+    last_name = StringField('last name', validators=[InputRequired(),
+        Length(min=3, max=90, message="Minimum length 3 maximum length of 90 characters")])
+    client_passport = StringField('client passport', validators=[InputRequired(),
+        Length(min=4, max=90, message="The name have to be from 5 to 90 characters")])
+    register_date = DateField('order date', validators=[InputRequired()])
+    submit = SubmitField('Add Client')
+
+    def validate_register_date(form, field):
+        if not field.data > datetime.date.today():
+            raise ValidationError('The day must be no less then current')
