128e55ab6a3aa3c46648962ceec810a4f47913ee
Ilya <itroshchinskiy@rambler.ua>
2021-06-10 20:42:52 +0300

    changed database

--- rentcars/config.py
+++ rentcars/config.py
@@ -1,7 +1,8 @@
 class Configuration:
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:@localhost/rentcars'
+    # SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:@localhost/rentcars'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///rentcars.db'
     SECRET_KEY = 'something_very_secret'
     SECURITY_PASSWORD_SALT = 'salt'
     SECURITY_PASSWORD_HASH = 'sha512_crypt'
