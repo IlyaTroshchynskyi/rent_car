6f83f96473d7c537d7353458ed359fc5e7d30bfc
Ilya <itroshchinskiy@rambler.ua>
2021-06-10 20:43:34 +0300

    updated models

--- rentcars/models.py
+++ rentcars/models.py
@@ -1,35 +1,37 @@
 from app import db
 from datetime import datetime
 
-
 class Orders(db.Model):
+
     __tablename__ = 'orders'
     id = db.Column(db.Integer, primary_key=True)
     car_number = db.Column(db.String(10))
+    car_description = db.Column(db.String(50), nullable=False)
     client_passport = db.Column(db.String(10))
-    date_rent = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    date_rent = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     rental_time = db.Column(db.Integer, nullable=False)
     rental_cost = db.Column(db.Float)
     total_cost = db.Column(db.Float)
-    car = db.relationship('Cars', backref='order_cars', lazy=True)
-    # clients = db.relationship('Clients', backref='clients', lazy=True)
+    car = db.relationship('Cars', backref='cars', lazy=True)
+    clients = db.relationship('Clients', backref='clients', lazy=True)
 
 
-# class Clients(db.Model):
-#     client_passport = db.Column(db.String(10), db.ForeignKey('orders.client_passport'),
-#                                 primary_key=True)
-#     first_name = db.Column(db.String(50), nullable=False)
-#     last_name = db.Column(db.String(50), nullable=False)
-#     registration_date = db.Column(db.DateTime, default=datetime.now())
-#     my_orders = db.relationship()
-
- # order1 = Orders(car_number="AX333333",client_passport="AH444444",rental_time=4,rental_cost=45,total_cost=180)
 class Cars(db.Model):
+
     __tablename__ = 'cars'
     id = db.Column(db.Integer, primary_key=True)
     car_number = db.Column(db.String(10), db.ForeignKey('orders.car_number'), unique=True)
-
     car_description = db.Column(db.String(50), nullable=False)
     rental_cost = db.Column(db.Float)
-    count_orders = db.Column(db.Integer)
+    number_orders = db.Column(db.Integer, default=0)
 
+
+class Clients(db.Model):
+
+    __tablename__ = 'clients'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    passport = db.Column(db.String(10), db.ForeignKey('orders.client_passport'), unique=True)
+    register_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    number_orders = db.Column(db.Integer, default=0)
