fd6f38aa5640808779a9a22388d1822ce8da7389
Ilya <itroshchinskiy@rambler.ua>
2021-06-17 09:17:24 +0300

    updated database

--- rentcars/models.py
+++ rentcars/models.py
@@ -2,29 +2,63 @@ from datetime import datetime
 from rentcars import db
 
 
+# class Orders(db.Model):
+#
+#     __tablename__ = 'orders'
+#     id = db.Column(db.Integer, primary_key=True)
+#     car_number = db.Column(db.String(10))
+#     car_description = db.Column(db.String(50), nullable=False)
+#     client_passport = db.Column(db.String(10))
+#     date_rent = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+#     rental_time = db.Column(db.Integer, nullable=False)
+#     rental_cost = db.Column(db.Float)
+#     total_cost = db.Column(db.Float)
+#     car = db.relationship('Cars', backref='cars', lazy=True)
+#     clients = db.relationship('Clients', backref='clients', lazy=True)
+#
+#
+# class Cars(db.Model):
+#
+#     __tablename__ = 'cars'
+#     id = db.Column(db.Integer, primary_key=True)
+#     car_number = db.Column(db.String(10), db.ForeignKey('orders.car_number'), unique=True)
+#     car_description = db.Column(db.String(50), nullable=False)
+#     rental_cost = db.Column(db.Float)
+#     number_orders = db.Column(db.Integer, default=0)
+#
+#
+# class Clients(db.Model):
+#
+#     __tablename__ = 'clients'
+#     id = db.Column(db.Integer, primary_key=True)
+#     first_name = db.Column(db.String(50), nullable=False)
+#     last_name = db.Column(db.String(50), nullable=False)
+#     passport = db.Column(db.String(10), db.ForeignKey('orders.client_passport'), unique=True)
+#     register_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+#     number_orders = db.Column(db.Integer, default=0)
+
 class Orders(db.Model):
 
     __tablename__ = 'orders'
     id = db.Column(db.Integer, primary_key=True)
-    car_number = db.Column(db.String(10))
+    car_number = db.Column(db.String(10), db.ForeignKey('cars.car_number'))
     car_description = db.Column(db.String(50), nullable=False)
-    client_passport = db.Column(db.String(10))
+    client_passport = db.Column(db.String(10), db.ForeignKey('clients.passport'))
     date_rent = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     rental_time = db.Column(db.Integer, nullable=False)
     rental_cost = db.Column(db.Float)
     total_cost = db.Column(db.Float)
-    car = db.relationship('Cars', backref='cars', lazy=True)
-    clients = db.relationship('Clients', backref='clients', lazy=True)
 
 
 class Cars(db.Model):
 
     __tablename__ = 'cars'
     id = db.Column(db.Integer, primary_key=True)
-    car_number = db.Column(db.String(10), db.ForeignKey('orders.car_number'), unique=True)
+    car_number = db.Column(db.String(10), unique=True)
     car_description = db.Column(db.String(50), nullable=False)
     rental_cost = db.Column(db.Float)
     number_orders = db.Column(db.Integer, default=0)
+    order = db.relationship('Orders', backref='car', lazy=True)
 
 
 class Clients(db.Model):
@@ -33,6 +67,7 @@ class Clients(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
-    passport = db.Column(db.String(10), db.ForeignKey('orders.client_passport'), unique=True)
+    passport = db.Column(db.String(10), unique=True)
     register_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     number_orders = db.Column(db.Integer, default=0)
+    order = db.relationship('Orders', backref='client', lazy=True)
