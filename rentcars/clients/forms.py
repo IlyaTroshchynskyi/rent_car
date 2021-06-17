990a636d90c192882e275e4b84783217370bea48
Ilya <itroshchinskiy@rambler.ua>
2021-06-17 07:40:44 +0300

    added blueprint clients

---
+++ rentcars/clients/forms.py
@@ -0,0 +1,19 @@
+import datetime
+from wtforms import StringField
+from wtforms.validators import InputRequired, Length, ValidationError
+from wtforms.fields.html5 import DateField
+from flask_wtf import FlaskForm
+
+
+class AddClient(FlaskForm):
+    first_name = StringField('First name', validators=[InputRequired(),
+        Length(min=3, max=90,message="First name have to be from 3 to 45 characters")])
+    last_name = StringField('Last name', validators=[InputRequired(),
+        Length(min=3, max=90, message="Last name have to be from 3 to 45 characters")])
+    client_passport = StringField('Client Passport', validators=[InputRequired(),
+        Length(min=4, max=10, message="The passport have to be from 4 to 10 characters")])
+    register_date = DateField('Register Date', validators=[InputRequired()])
+
+    def validate_register_date(form, field):
+        if not field.data >= datetime.date.today():
+            raise ValidationError('The day must be no less then current')
---
+++ rentcars/clients/routes.py
@@ -0,0 +1,64 @@
+from datetime import datetime
+from flask import render_template, url_for, redirect, flash, request, Blueprint
+from rentcars import db
+from rentcars.models import Clients
+from rentcars.clients.forms import AddClient
+
+
+clients = Blueprint('clients', __name__)
+
+
+@clients.route("/clients", methods=['GET', 'POST'])
+def show_clients():
+    if request.method == 'POST':
+        start = datetime.strptime(request.form['calendar_start'], "%Y-%m-%d").date()
+        end = datetime.strptime(request.form['calendar_end'], "%Y-%m-%d").date()
+        clients = Clients.query.filter(Clients.register_date.between(start, end))
+        return render_template('clients.html', clients=clients, start=start, end=end)
+    start = datetime.strptime('31-12-1970', "%d-%m-%Y").date()
+    end = datetime.strptime('31-12-2100', "%d-%m-%Y").date()
+    clients = Clients.query.all()
+    return render_template('clients.html', clients=clients, start=start, end=end)
+
+
+@clients.route("/add_client", methods=['GET', 'POST'])
+def add_client():
+    form = AddClient()
+    client = Clients.query.filter_by(passport=form.client_passport.data).first()
+    if form.validate_on_submit() and client is None:
+        client = Clients(first_name=form.first_name.data, last_name=form.last_name.data,
+                         passport=form.client_passport.data, register_date=form.register_date.data)
+        db.session.add(client)
+        db.session.commit()
+        flash('The client was successfully added', 'success')
+        return redirect(url_for('clients.show_clients'))
+    return render_template('add_client.html', form=form, title='Add client')
+
+
+@clients.route("/update_client/<client_id>", methods=['GET', 'POST'])
+def update_client(client_id):
+    client = Clients.query.filter_by(id=client_id).first()
+    form = AddClient()
+    if form.validate_on_submit():
+        client.first_name = form.first_name.data
+        client.last_name = form.last_name.data
+        client.passport = form.client_passport.data
+        client.register_date = form.register_date.data
+        db.session.commit()
+        flash('The client was successfully updated', 'success')
+        return redirect(url_for('clients.show_clients'))
+    if request.method == 'GET':
+        form.first_name.data = client.first_name
+        form.last_name.data = client.last_name
+        form.client_passport.data = client.passport
+        form.register_date.data = client.register_date
+    return render_template('add_client.html', form=form, title='Update client')
+
+
+@clients.route("/delete_client/<client_id>", methods=['GET', 'POST'])
+def delete_client(client_id):
+    client = Clients.query.get_or_404(client_id)
+    db.session.delete(client)
+    db.session.commit()
+    flash('Client was deleted successfully', 'success')
+    return redirect(url_for('clients.show_clients'))
