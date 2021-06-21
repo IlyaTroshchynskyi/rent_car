a61201404ab78300ca6fd2ca59e7a8450ab87f9d
Ilya <itroshchinskiy@rambler.ua>
2021-06-21 18:25:03 +0300

    added docstrings and uodate code according pep8

--- rentcars/cars/forms.py
+++ rentcars/cars/forms.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+"""forms
+   Implements the forms for cars
+"""
 from wtforms import StringField, FloatField
 from wtforms.validators import InputRequired, Length, NumberRange, ValidationError
 from flask_wtf import FlaskForm
@@ -5,6 +9,8 @@ from rentcars.models import Cars
 
 
 class AddCar(FlaskForm):
+    """Form for adding new car to database or updating information about car
+    """
 
     car_number = StringField('Car Number', validators=[InputRequired(
         message="Data for cars number have to be provided")])
@@ -14,6 +20,12 @@ class AddCar(FlaskForm):
         NumberRange(min=1, message="Should contain numbers from 1")])
 
     def validate_car_number(form, field):
+        """Validate car number. Car number must be unique
+        Args:
+            field (str): Car number
+        Raises:
+            ValidationError
+        """
         car = Cars.query.filter_by(car_number=field.data).first()
         if car is not None:
             raise ValidationError('This cars is exist in database')
